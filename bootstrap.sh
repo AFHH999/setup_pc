1a30850b8158dbe13c08e1d4c9c474431b351383
Felipe <ah193971@gmail.com>
2025-12-01 22:24:02 -0600

    New changes added

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,23 @@
 # Geting git and ansible
 echo "installing git and ansible...."
 sudo pacman -Syu --nocorfirm ansible git
+
+# Set up the directory
+REPO_URL="https://github.com/AFHH999/setup_pc"
+TARGET_DIR="$HOME/PC_setup/"
+
+if [ -d "TARGET_DIR" ]; then
+    echo "directory $TARGET_DIR already exist. Pulling latest changes..."
+    cd "TARGET_DIR" || exit
+    git pull
+
+else
+    echo "Cloning repository..."
+    git clone "$REPO_URL" "$TARGET_DIR"
+    cd "$TARGET_DIR" || exit
+fi
+
+#Running the playbook
+echo "Running the ansible playbook..."
+ansible-playbook -i inventory.ini setup.yml --ask-become-pass
+echo "Setup complete! Please restart your shell or log out and back in."
